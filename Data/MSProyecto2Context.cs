93b29ea83d9ebfa870ea506480558b6d39fc029f
laboratorio <laboratorio@UPEAULA-31708>
2024-10-07 11:25:52 -0500

    Agregar archivos de proyecto.

---
+++ Data/MSProyecto2Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MSProyecto2.Models;
+
+    public class MSProyecto2Context : DbContext
+    {
+        public MSProyecto2Context (DbContextOptions<MSProyecto2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MSProyecto2.Models.Burger> Burger { get; set; } = default!;
+    }
